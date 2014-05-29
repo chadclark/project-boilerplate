801281458f36cfd98b033034733f02a740130695
Chad Clark <chad@chadjclark.com>
2014-05-29 17:27:13 -0400

    Gulping all the things

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,10 +1,14 @@
 // Set Plugin Variables
 var gulp = require('gulp') // Gulp!
+	changed 	= require('gulp-changed'), // Watch for changed files
 	concat		= require('gulp-concat'), // Concat
+	debug 		= require('gulp-debug'), // Debug
 	fileInclude	= require('gulp-file-include'), // File Include
+	imagemin 	= require('gulp-imagemin'), // Image Minifying
 	jshint		= require('gulp-jshint'), // JS Hinting
 	livereload 	= require('gulp-livereload'), // Live Reload
 	notify		= require('gulp-notify'), // Notify
+	pngcrush 	= require('imagemin-pngcrush'); // PNG Crush
 	rename		= require('gulp-rename'), // Rename
 	sass		= require('gulp-ruby-sass'), // Sass (We have to use Ruby Sass until LibSass supports 3.3)
 	uglify		= require('gulp-uglify'), // Uglify JS
@@ -15,8 +19,15 @@ var gulp = require('gulp') // Gulp!
 var paths = {
 	build: './assets/build/',
 	js: './assets/js/',
-	scss: './assets/scss/'
+	scss: './assets/scss/',
+	img: './assets/img/'
 };
+
+// Error Logging
+function handleError(err) {
+	console.log(err.toString());
+	this.emit('end');
+}
 
 // JS Hint
 gulp.task('jshint', function() {
@@ -53,19 +64,39 @@ gulp.task('styles', function() {
 			sourcemap: true,
 			style: 'compressed'
 		}))
+		//.pipe(debug({verbose: true}))
+		.on('error', handleError)
 		.pipe(gulp.dest(paths.build + 'css/'))
 		.pipe(notify('SCSS Processed'))
 });
 
+// Image Minifying
+gulp.task('imagemin', function() {
+	var imgSrc = paths.img + '**/*',
+		imgDst = paths.build + 'img/'
+	;
+
+	gulp.src(imgSrc)
+		.pipe(changed(imgSrc))
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngcrush()]
+		}))
+		.pipe(gulp.dest(imgDst))
+		.pipe(notify('Images Optimized'))
+});
+
 // Watch
 gulp.task('watch', function() {
-	gulp.watch(paths.js + '*.js', ['jshint', 'scripts']);
-	gulp.watch(paths.js + 'plugins.js', ['fileInclude']);
-	gulp.watch(paths.scss + '*.scss', ['styles']);
+	gulp.watch(paths.js + 'main.js', ['jshint', 'scripts']);
+	gulp.watch(paths.js + 'plugins.js', ['fileInclude', 'scripts']);
+	gulp.watch(paths.scss + '**/*.scss', ['styles']);
+	gulp.watch(paths.img + '**/*.*', ['imagemin']);
 	gulp.watch(paths.build + '**').on('change', function(file) {
 		livereload().changed(file.path);
 	}); // Live reload if anything in /assets/build changes
 });
 
 // Default Task
-gulp.task('default', ['jshint', 'styles', 'scripts', 'fileInclude', 'watch']);
+gulp.task('default', ['jshint', 'styles', 'scripts', 'imagemin', 'fileInclude', 'watch']);
