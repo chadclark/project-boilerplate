2827861e679cae77226d7b3cf8369b752229fb3d
Chad Clark <chad@iMac.local>
2017-04-23 12:58:09 -0400

    Getting webpack into the action.

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,70 @@ const $ = require('gulp-load-plugins')({
 	scope: ['devDependencies']
 });
 
+const webpackStream = require('webpack-stream');
+const webpack = require('webpack');
+
 // read in package vars from `package.json`
 const pkg = require('./package.json');
 
 // Browsersync
 const browserSync = $.browserSync.create();
 
+const runSequence  = require('run-sequence').use(gulp);
+
+let isProd = true;
+
 // Auto Prefixer Options
 const autoprefixerOptions = {
 	browsers: [
-		'last 2 versions'
+		'ie >= 9',
+		'safari >= 7',
+		'ios >= 7',
+		'android >= 4',
 	],
 	cascade: false
 };
+
+const webpackConfig = {
+	watch: true,
+	output: {
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ [
+								'es2015',
+								{
+									'targets': {
+										'browsers': [
+											'last 2 versions',
+											'ie >= 9'
+										]
+									},
+									'loose': true,
+									'modules': false
+								}
+							] ],
+							plugins: [
+								// for IE9
+								// see https://gist.github.com/zertosh/4f818163e4d68d58c0fa
+								'transform-proto-to-assign'
+								// 'transform-object-assign'
+							]
+						}
+					}
+				]
+			}
+		]
+	}
+}
 
 gulp.task('browser-sync', function() {
     return browserSync.init({
@@ -47,7 +98,18 @@ gulp.task('scss', function() {
 	;
 });
 
+gulp.task('js', function() {
+	return gulp.src(pkg.paths.src.js + 'main.js')
+	.pipe(webpackStream(webpackConfig, webpack))
+	.pipe(gulp.dest(pkg.paths.dist.js))
+	.pipe($.rename({ extname: '.min.js' }))
+	.pipe(gulp.dest(pkg.paths.dist.js))
+	.pipe(browserSync.reload({stream: true}))
+	;
+});
+
 gulp.watch(pkg.paths.src.scss + '**/*.scss', ['scss']);
+gulp.watch(pkg.paths.src.js + '**/*.js', ['js']);
 gulp.watch(pkg.paths.templates.path + '**/*' + pkg.paths.templates.ext).on('change', browserSync.reload);
 
-gulp.task('default', ['browser-sync', 'scss']);
+gulp.task('default', ['browser-sync', 'scss', 'js']);
